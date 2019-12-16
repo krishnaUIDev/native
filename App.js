3af0ea21ff637b7d45897f7b98f9052030e3e527
krishnaUIDev <krishna.kondoju007@gmail.com>
2019-12-15 20:21:43 -0500

    paper added

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import React, { useState } from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native'
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
-export default function App () {
-  const [value, setButton] = useState('hey you clicked me')
+export default function App() {
+  const [value, setButton] = useState("hey you clicked me");
   return (
     <View style={styles.container}>
       <Text>{value}</Text>
-      <Button title={'Click me'} onPress={() => setButton('hey')} />
+      <Button title={"jh"} onPress={() => setButton("hey")} />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   }
-})
+});
